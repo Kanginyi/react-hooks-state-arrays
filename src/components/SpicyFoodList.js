b76a10ce70ddea3ab139c760ff81db292e67bb9a
Eric Yi <erickanginyi@gmail.com>
2021-09-24 08:06:33 -0400

    commit tree 47f0c1fc170a34f150bbd75062aa6c964816ea8d

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,18 +3,46 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  //foods state, setFood methods, spicyFoods array
+
+  const foodList = foods.map(food => {
+    <li key={food.id}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  })
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{foodList}</ul>
     </div>
   );
 }
 
 export default SpicyFoodList;
+
+/**
+const spicyFoods = [
+  { id: 1, name: "Buffalo Wings", cuisine: "American", heatLevel: 3 },
+  { id: 2, name: "Mapo Tofu", cuisine: "Sichuan", heatLevel: 6 },
+];
+
+let nextId = 3;
+
+const newSpicyFoods = [
+  { name: "Green Curry", cuisine: "Thai", heatLevel: 9 },
+  { name: "Enchiladas", cuisine: "Mexican", heatLevel: 2 },
+  { name: "5 Alarm Chili", cuisine: "American", heatLevel: 5 },
+];
+
+getNewSpicyFood will return a new spicy food from newSpicyFoods array
+
+export { spicyFoods, getNewSpicyFood };
+
+ */
